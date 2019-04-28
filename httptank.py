dbe604fe3fef7e27d97db11da76c002755f0d7c9
takasi7 <zetecrpc@outlook.com>
2019-04-28 11:27:16 +0900

    renew webUI

--- httptank.py
+++ httptank.py
@@ -5,64 +5,18 @@ from urllib.parse import urlparse
 
 rctank = Tank()
 
-htmltemplate = '''<html>
-<head>
-<title>RC TANK TA7291P</title>
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<script>
-function btnact(id) {
-	var obj = document.getElementById("cmd");
-	var objfm = document.getElementById("tankaction");
-	obj.value = id;
-	objfm.submit();
-}
+htmltemplate = '<html><head></head><body>Hello,World</body></html>'
 
-function tankspeed(speed) {
-	var obj = document.getElementById("speed");
-	var objfm = document.getElementById("tankaction");
-	obj.value = speed;
-	objfm.submit();
-}
+def load_template():
+	global htmltemplate
+	try:
+		f = open('/home/pi/TA7291P/template.html')
+	except:
+		return
+	htmltemplate = f.read()
+	f.close()
+	return
 
-</script>
-</head>
-<body>
-<table>
-<tr>
-	<td></td>
-	<td><input type="button" value="forward" id="forward" onclick="btnact(this.id)"></td>
-	<td></td>
-</tr>
-<tr>
-	<td><input type="button" value="left" id="leftturn" onclick="btnact(this.id)"></td>
-	<td><input type="button" value="stop" id="brake" onclick="btnact(this.id)"></td>
-	<td><input type="button" value="right" id="rightturn" onclick="btnact(this.id)"></td>
-</tr>
-
-<tr>
-	<td></td>
-	<td><input type="button" value="back" id="back" onclick="btnact(this.id)"></td>
-	<td></td>
-</tr>
-</table>
-<div>
-<input type="radio" name="speed" onclick="tankspeed(50)" __50__ /><label>1  </label>
-<input type="radio" name="speed" onclick="tankspeed(60)" __60__ /><label>2  </label>
-<input type="radio" name="speed" onclick="tankspeed(70)" __70__ /><label>3  </label>
-<input type="radio" name="speed" onclick="tankspeed(80)" __80__ /><label>4  </label>
-<input type="radio" name="speed" onclick="tankspeed(90)" __90__ /><label>5  </label>
-<input type="radio" name="speed" onclick="tankspeed(100)" __100__  /><label>MAX</label>
-</div>
-<img src="http://10.0.1.201:8080/?action=stream">
-<!--
-<iframe src="http://10.0.1.201:8080/javascript_simple.html" width="640" height="480"></iframe>
--->
-<form id="tankaction" action="index.html" method="get" enctype="application/x-www-form-urlencoded">
-<input type="hidden" name="cmd" value="None" id="cmd">
-<input type="hidden" name="speed" value="__SPEED__" id="speed">
-</form>
-</body>
-</html>'''
 
 class HelloHandler(BaseHTTPRequestHandler):
 	def parse_query(self):
@@ -101,19 +55,33 @@ class HelloHandler(BaseHTTPRequestHandler):
 				return -1
 			if sp >= 50 and sp <= 100 and sp != rctank.speed:
 				rctank.setspeed(sp)
-				
 		return 0
-
+	def state2cmd(self,state):
+		command = 'None'
+		if state == 0:
+			command = 'brake'
+		elif state == 1:
+			command = 'acceloff'
+		elif state == 2:
+			command = 'forward'
+		elif state == 3:
+			command = 'rightturn'
+		elif state == 4:
+			command = 'back'
+		elif state == 5:
+			command = 'leftturn'
+		return command
 	def gen_html(self,qc):
 		global htmltemplate
 		global rctank
 		html = htmltemplate
 		html = html.replace('__SPEED__',str(rctank.speed))
-		chtag = "__"+str(rctank.speed)+"__"
-		html = html.replace(chtag, "checked")
-		for s in range(50,110,10):
-			s = '__'+str(s)+'__'
-			html = html.replace(s,'')
+		html = html.replace('__COMMAND__', self.state2cmd(rctank.state))
+		#chtag = "__"+str(rctank.speed)+"__"
+		#html = html.replace(chtag, "checked")
+		#for s in range(50,110,10):
+		#	s = '__'+str(s)+'__'
+		#	html = html.replace(s,'')
 		return html
 	
 	def do_GET(self):
@@ -129,6 +97,7 @@ class HelloHandler(BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
+	load_template()
 	server_address = ('', 8000)
 	httpd = HTTPServer(server_address, HelloHandler)
 	httpd.serve_forever()
--- tank.py
+++ tank.py
@@ -7,6 +7,7 @@ class Tank():
 		self.left = ta7291p.TankMotor(5,6,12)
 		self.right = ta7291p.TankMotor(19,26,13)
 		self.speed = 50
+		self.state = 0
 		self.setspeed(self.speed)
 		return
 	def setspeed(self, myspeed):
@@ -17,25 +18,31 @@ class Tank():
 	def leftturn(self):
 		self.left.accelon(back=True)
 		self.right.accelon()
+		self.state = 5
 		return
 	def rightturn(self):
 		self.left.accelon()
 		self.right.accelon(back=True)
+		self.state = 3
 		return
 	def forward(self):
 		self.left.accelon()
 		self.right.accelon()
+		self.state = 2
 		return
 	def back(self):
 		self.left.accelon(back=True)
 		self.right.accelon(back=True)
+		self.state = 4
 		return
 	def brake(self):
 		self.left.brakeon()
 		self.right.brakeon()
+		self.state = 0
 		return
 	def acceloff(self):
 		self.left.acceloff()
 		self.right.acceloff()
+		self.state = 1
 		return
 
